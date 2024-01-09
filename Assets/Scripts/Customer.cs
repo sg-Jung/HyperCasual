4c906c4a7e2bd1f191c4bd8b78360f0db0f4488e
SangGyeongJeong <alow9942@daum.net>
2024-01-10 01:06:39 +0900

    2024-01-09

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -15,6 +15,8 @@ public class Customer : MonoBehaviour
 
     public Stack<GameObject> customerStack = new Stack<GameObject>();
     public int neededBreadCount;
+    public float moveAnimDuration;
+
     public bool isStacking;
     public bool isMove;
     public bool isTalking;
@@ -46,18 +48,44 @@ public class Customer : MonoBehaviour
         anim.SetBool("isTalking", isTalking);
     }
 
+    void SetQuestState(QuestState newState) // 상태 변경 시 한 번 수행
+    {
+        currentQuestState = newState;
+        Debug.Log("퀘스트 상태 변경: " + newState);
+
+        // 상태에 따른 동작 수행
+        if (newState == QuestState.TakeBread)
+        {
+            Debug.Log("빵을 가져가세요.");
+            int random = Random.Range(1, 4);
+            neededBreadCount = random;
+            // 말풍선에 neededBreadCount갯수 넣는 코드 추가
+
+            isMove = true;
+        }
+        else if (newState == QuestState.PayAtCheckout)
+        {
+            Debug.Log("계산대에서 계산하세요.");
+        }
+        else if (newState == QuestState.EatInRestaurant)
+        {
+            Debug.Log("식당에서 식사하세요.");
+        }
+        else if (newState == QuestState.Leave)
+        {
+            Debug.Log("식당을 떠나세요.");
+        }
+    }
+
     void CustomerState()
     {
         switch (currentQuestState)
         {
             case QuestState.TakeBread:
-                // 빵 진열대에서 빵을 가져가는 동작
-                if (Input.GetKeyDown(KeyCode.G))  // 가상의 키 G를 누를 때
-                {
-                    TakeBreadFromShelf();
-                }
+                
                 break;
 
+    
             case QuestState.PayAtCheckout:
                 if (Input.GetKeyDown(KeyCode.P))
                 {
@@ -78,32 +106,6 @@ public class Customer : MonoBehaviour
                     Debug.Log("퀘스트 완료: 빵을 가져가고, 계산하고, 식당에서 먹고 나가기");
                 }
                 break;
-        }
-    }
-
-    void SetQuestState(QuestState newState) // 상태 변경 시 한 번 수행
-    {
-        currentQuestState = newState;
-        Debug.Log("퀘스트 상태 변경: " + newState);
-
-        // 상태에 따른 동작 수행
-        if (newState == QuestState.TakeBread)
-        {
-            Debug.Log("빵을 가져가세요.");
-            isMove = true;
-
-        }
-        else if (newState == QuestState.PayAtCheckout)
-        {
-            Debug.Log("계산대에서 계산하세요.");
-        }
-        else if (newState == QuestState.EatInRestaurant)
-        {
-            Debug.Log("식당에서 식사하세요.");
-        }
-        else if (newState == QuestState.Leave)
-        {
-            Debug.Log("식당을 떠나세요.");
         }
     }
 
